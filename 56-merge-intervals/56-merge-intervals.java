9b6182daa88dc462c7152eb3a76665b053f8d0ac
MSayedz <36796596+MSayedz@users.noreply.github.com>
2022-07-19 20:12:17 +0200

    Time: 8 ms (94.91%), Space: 47.8 MB (84.24%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -5,6 +5,7 @@ class Solution {
         LinkedList<int[]> merged = new LinkedList<>();
         int s = intervals[0][0];
         int e = intervals[0][1];
+        
         for (int i = 1; i<intervals.length;i++) {
             if ( e < intervals[i][0]) {
                 merged.add(new int[]{s,e});
