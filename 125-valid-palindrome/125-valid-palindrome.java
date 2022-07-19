48f4cb8cfaf2987b9709ba7c8b56ad2ef5c6ce71
MSayedz <36796596+MSayedz@users.noreply.github.com>
2022-07-19 19:18:12 +0300

    Time: 892 ms (20.55%), Space: 47.6 MB (19.33%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,21 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
-         String actual = 
+        String actual = 
              s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
-        String rev = new StringBuffer(actual)
-            .reverse().toString();
+        String rev = new StringBuffer(actual).reverse().toString();
         return actual.equals(rev);
-        
-        // char[] c = s.toCharArray();
-        // for (int i = 0, j = c.length - 1; i < j; ) {
-        //     if (!Character.isLetterOrDigit(c[i])) 
-        //         i++;
-        //     else if (!Character.isLetterOrDigit(c[j])) 
-        //         j--;
-        //     else if (Character.toLowerCase(c[i++]) 
-        //              != Character.toLowerCase(c[j--])) 
-        //         return false;
-        // }
-        // return true;
+        /* 
+        char[] c = s.toCharArray();
+        for (int i = 0, j = c.length - 1; i < j; ) {
+            if (!Character.isLetterOrDigit(c[i])) 
+                i++;
+            else if (!Character.isLetterOrDigit(c[j])) 
+                j--;
+            else if (Character.toLowerCase(c[i++]) 
+                     != Character.toLowerCase(c[j--])) 
+                return false;
+        }
+        return true;
+        */
     }
 }
