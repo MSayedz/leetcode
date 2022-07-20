0fbf570b4c8e94acc7686c821abcbd7307215761
MSayedz <36796596+MSayedz@users.noreply.github.com>
2022-07-20 16:53:18 +0200

    Time: 3 ms (52.62%), Space: 77.2 MB (20.73%) - LeetHub

--- 860-lemonade-change/860-lemonade-change.java
+++ 860-lemonade-change/860-lemonade-change.java
@@ -4,23 +4,17 @@ class Solution {
         int ten = 0;
         
         for(int bill:bills){
-            if(bill == 5)
+            if(bill == 5){
                 ++five;
-            else if(bill == 10){
+            } else if(bill == 10){
                 ++ten;
-                if(five == 0)
-                    return false;
+                if(five == 0){return false;}
                 --five;
-            } else if(bill == 20){
-                if(five == 0)
-                    return false;
-                if(ten == 0 && five < 3)
-                    return false;
-                --five;
-                if(ten != 0)
-                    --ten;
-                else
-                    five -=2;
+            } else {
+                if(five == 0){return false;}
+                if(ten == 0 && five < 3){return false;}
+                if(ten != 0){--ten;--five;}
+                else{five -=3;}
             }
         }
         return true;
