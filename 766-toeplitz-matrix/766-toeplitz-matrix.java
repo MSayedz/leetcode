f7bf28286192153e5186e7c3cf169f0a089c525b
MSayedz <36796596+MSayedz@users.noreply.github.com>
2022-07-23 18:48:08 +0200

    Time: 1 ms (94.46%), Space: 45.9 MB (48.96%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.java
+++ 766-toeplitz-matrix/766-toeplitz-matrix.java
@@ -4,6 +4,7 @@ class Solution {
             for (int j=0;j<matrix[0].length-1;j++)
                 if(matrix[i][j] != matrix[i+1][j+1])
                     return false;
+        
         return true;
     }
 }
