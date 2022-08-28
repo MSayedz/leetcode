cb650b8875b465398340c8cf690d2ab320f05115
MSayedz <36796596+MSayedz@users.noreply.github.com>
2022-08-28 15:33:13 +0300

    Time: 0 ms (100.00%), Space: 45.1 MB (31.60%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.java
+++ 832-flipping-an-image/832-flipping-an-image.java
@@ -1,6 +1,27 @@
 class Solution {
-    public int[][] flipAndInvertImage(int[][] image) {
-        int rowSize = image[0].length;
+    public int[][] flipAndInvertImage(int[][] A) {
+        for(int i=0; i<A[0].length; i++){
+            reverse(A[i]);
+        }
+        return A;
+    }
+
+    public void reverse(int[] A){
+        int start = 0;
+        int end = A.length - 1;
+        while(start<=end){
+            int temp = A[start];
+            A[start] = invert(A[end]);
+            A[end] =invert(temp);
+            start++;
+            end--;
+        }
+    }
+    public int invert(int i){
+        return i == 0 ? 1 :0;
+    }
+    /*
+    int rowSize = image[0].length;
         for(int i=0;i<image.length;i++){
             for(int j=0;j<rowSize/2;j++){
                 int temp = image[i][j];
@@ -15,5 +36,5 @@ class Solution {
             }
         }
         return image;
-    }
+    */
 }
