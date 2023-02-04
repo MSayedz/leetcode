119cc2c13a222776ba6ec93c91674fc13e1c39d6
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-02-05 01:00:52 +0300

    Time: 2 ms (62.56%), Space: 40.3 MB (63.81%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,18 +1,33 @@
 class Solution {
     public boolean isValid(String s) {
-        Map<Character,Character> map =  new HashMap<>();
-        map.put('(',')');
-        map.put('[',']');
-        map.put('{','}');
         
-        Deque<Character> stack = new ArrayDeque<>();
-        for(int i = 0;i<s.length();i++){
-            if(map.containsKey(s.charAt(i)))
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') 
                 stack.push(s.charAt(i));
-            else if(stack.isEmpty() || map.get(stack.pop()) != s.charAt(i))
-                return false;
+            else {
+                if (stack.isEmpty()) return false;
+
+                if (s.charAt(i) == ')' && stack.pop() != '(') return false;
+                else if (s.charAt(i) == ']' && stack.pop() != '[') return false; 
+                else if (s.charAt(i) == '}' && stack.pop() != '{') return false;
+            }
         }
-        
+
         return stack.isEmpty();
+//         Map<Character,Character> map =  new HashMap<>();
+//         map.put('(',')');
+//         map.put('[',']');
+//         map.put('{','}');
+        
+//         Deque<Character> stack = new ArrayDeque<>();
+//         for(int i = 0;i<s.length();i++){
+//             if(map.containsKey(s.charAt(i)))
+//                 stack.push(s.charAt(i));
+//             else if(stack.isEmpty() || map.get(stack.pop()) != s.charAt(i))
+//                 return false;
+//         }
+        
+//         return stack.isEmpty();
     }
 }
