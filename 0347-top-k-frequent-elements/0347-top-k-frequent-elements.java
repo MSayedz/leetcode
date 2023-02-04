cfd88178f6a2ba2a1884d45c06c354f121c3a4fa
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-02-04 17:59:30 +0300

    Time: 17 ms (35.87%), Space: 45.1 MB (53.59%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -20,14 +20,15 @@ class Solution {
 //         }
 //         return res;
         PriorityQueue<Map.Entry<Integer, Integer>> pq =
-            new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+            new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
 
-        for (Map.Entry entry : map.entrySet())
+        for (Map.Entry entry : map.entrySet()){
             pq.add(entry);
-
-        int[] ans = new int[k];
-        for (int i = 0; i < k; i++)
-            ans[i] = pq.poll().getKey();
-        return ans;
+            if (pq.size() > k)
+                pq.poll();
+        }
+        
+        return Stream.generate(pq::poll).limit(pq.size())
+            .map(x -> x.getKey()).mapToInt(Integer::intValue).toArray();
     }
 }
