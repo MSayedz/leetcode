afd62e5da02233545403511e7877fcd4d4be1350
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-02-05 16:08:25 +0100

    Time: 0 ms (100.00%), Space: 41.9 MB (52.42%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -11,7 +11,7 @@ class Solution {
         if(node.next != null){
             node.val = node.next.val;
             node.next = node.next.next;
-        } else{
+        } else {
             node = null;
         }
     }
