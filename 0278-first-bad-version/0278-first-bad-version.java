9eb1dc7f68ba754a64624a42fc6b2a503850d314
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-02-05 15:54:58 +0300

    Time: 15 ms (99.93%), Space: 39.1 MB (66.57%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -7,10 +7,11 @@ public class Solution extends VersionControl {
         
         while(left<right){
             int mid = left + (right-left)/2;
-            if(isBadVersion(mid))
+            if(isBadVersion(mid)){
                 right = mid;
-            else
+            } else{
                 left = mid+1;
+            }
         }
         
         return left;
