d4f0e0406a7a9e41b103170a051c0cc7b6e00995
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-02-06 22:48:42 +0100

    Time: 4 ms (5.50%), Space: 42.6 MB (27.38%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {        
         int i=m-1,j=n-1;
         // for(int k=nums1.length-1;k>=0;k--){
         //     if(i >= 0 && (j < 0 || nums1[i] > nums2[j]))
@@ -8,13 +8,21 @@ class Solution {
         //         nums1[k] = nums2[j--];
         // }
         
-        int k = nums1.length-1;
-        while(i>=0 && j>=0)
-            if(nums1[i] < nums2[j])
-                nums1[k--] = nums2[j--];
-            else
-                nums1[k--] = nums1[i--];
-        while(j>=0)
-            nums1[k--] = nums2[j--];
+        // int k = nums1.length-1;
+        // while(i>=0 && j>=0)
+        //     if(nums1[i] < nums2[j])
+        //         nums1[k--] = nums2[j--];
+        //     else
+        //         nums1[k--] = nums1[i--];
+        // while(j>=0)
+        //     nums1[k--] = nums2[j--];
+        
+        // Java 8
+        System.arraycopy(IntStream.concat(
+                    Arrays.stream(nums1, 0, m),
+                    Arrays.stream(nums2, 0, n)
+                )
+                .sorted()
+                .toArray(), 0, nums1, 0, nums1.length);;
     }
 }
