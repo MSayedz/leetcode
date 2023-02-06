625cdc53571598421839952f05d7f408e95d8ee4
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-02-07 00:24:20 +0300

    Time: 1 ms (99.45%), Space: 42.3 MB (85.76%)

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,13 +2,24 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         
-        for(int i = 0;i < nums.length; i++){
-            if(map.containsKey(target - nums[i]))
-                return new int[] {i,map.get(target- nums[i])};
-             else 
-                map.put(nums[i], i);
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[] {i, map.get(target-nums[i])};
+            }
+            map.putIfAbsent(nums[i], i);
         }
         
-        return null;
+        return new int[2];
+        
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // return IntStream.range(0, nums.length)
+        //     .filter(i -> {
+        //         if(map.containsKey(nums[i])) return true;
+        //         map.put(target - nums[i], i);
+        //         return false;
+        //     })
+        //     .mapToObj(i -> new int[] {map.get(nums[i]), i})
+        //     .findFirst()
+        //     .orElse(new int[2]);
     }
 }
