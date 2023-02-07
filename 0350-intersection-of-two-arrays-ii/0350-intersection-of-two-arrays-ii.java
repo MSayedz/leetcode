6c069fc721f344b940cbfa7d89321a303c6c9963
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-02-07 14:25:20 +0100

    Time: 1 ms (99.02%), Space: 42.1 MB (96.32%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,43 +1,40 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Map<Integer,Integer> nums1Map = new HashMap<>();
-        
-        for(int num:nums1)
-            nums1Map.put(num,nums1Map.getOrDefault(num,0)+1);
-        
-        List<Integer> res = new ArrayList();
-        for(int num:nums2){
-            if(nums1Map.containsKey(num)){
-                if(nums1Map.get(num)>0 ){
-                    res.add(num);
-                    nums1Map.put(num,nums1Map.get(num)-1);
-                }
-            }
-        }
-        
-        int[] arr = new int[res.size()];
-        for(int k = 0; k < res.size();k++) 
-            arr[k] = res.get(k);
-        return arr;
-        
-        
-//         int[] count = new int[1001];
-//         int[] temp = new int[1001];
+//         Map<Integer,Integer> nums1Map = new HashMap<>();
         
 //         for(int num:nums1)
-//             count[num]++;
-
-//         List<Integer> list = new ArrayList();
+//             nums1Map.put(num,nums1Map.getOrDefault(num,0)+1);
+        
+//         List<Integer> res = new ArrayList();
 //         for(int num:nums2){
-//             if(count[num]>0){
-//                 list.add(num);
-//                 count[num]--;
+//             if(nums1Map.containsKey(num)){
+//                 if(nums1Map.get(num)>0){
+//                     res.add(num);
+//                     nums1Map.put(num,nums1Map.get(num)-1);
+//                 }
 //             }
 //         }
+        
+//         return res.stream().mapToInt(Integer::intValue).toArray();
+        
+        
+        int[] count = new int[1001];
+        int[] temp = new int[1001];
+        
+        for(int num:nums1)
+            count[num]++;
 
-//         int[] res = new int[list.size()];
-//         for(int k = 0; k < list.size();k++) 
-//             res[k] = list.get(k);
-//         return res;
+        List<Integer> list = new ArrayList();
+        for(int num:nums2){
+            if(count[num]>0){
+                list.add(num);
+                count[num]--;
+            }
+        }
+
+        int[] res = new int[list.size()];
+        for(int k = 0; k < list.size();k++) 
+            res[k] = list.get(k);
+        return res;
     }
 }
