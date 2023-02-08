88c84629b1a6bace09e27265c021c14f5be7803b
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-02-08 20:13:07 +0100

    Time: 0 ms (100.00%), Space: 43.4 MB (48.01%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.java
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -14,5 +14,20 @@ class Solution {
             }
         }
         return res;
+        
+//         int[][] res = new int[r][c];
+//         if (nums.length == 0 || r * c != nums.length * nums[0].length)
+//             return nums;
+        
+//         Queue<Integer> queue = new LinkedList();
+//         for (int i = 0; i < nums.length; i++) 
+//             for (int j = 0; j < nums[0].length; j++) 
+//                 queue.add(nums[i][j]);
+                
+//         for (int i = 0; i < r; i++) 
+//             for (int j = 0; j < c; j++) 
+//                 res[i][j] = queue.remove();
+        
+//         return res;
     }
 }
