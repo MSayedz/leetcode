33a78936de192fa01f78101a2dfb6ced3b6c5b57
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-02-10 22:58:06 +0100

    Time: 4 ms (86.19%), Space: 42.3 MB (46.91%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int n = s1.length(), m = s2.length();
-        int[] first = new int[26];
-        for(int i = 0;i < n;i++)
-            first[s1.charAt(i)-'a']++;
-        
-        int[] second = new int[26];
-        for(int i = 0;i < m;i++){
-            second[s2.charAt(i)-'a']++;
-            if(i >= n)
-                second[s2.charAt(i-n)-'a']--;
-            
-            if(Arrays.equals(first, second))
+        int len1 = s1.length();
+        int[] arr1 = new int[26];
+        for(int i=0;i<len1;i++){
+            arr1[s1.charAt(i)-'a']++;
+        }
+        int[] arr2 = new int[26];
+        for(int i = 0;i < s2.length();i++){
+            arr2[s2.charAt(i)-'a']++;
+            if(i >= len1){
+                arr2[s2.charAt(i-len1)-'a']--;
+            }
+            if(Arrays.equals(arr1, arr2)){
                 return true;
+            }
         }
         return false;
     }
