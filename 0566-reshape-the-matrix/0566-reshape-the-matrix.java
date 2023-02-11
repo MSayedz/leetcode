3ad9c1a4428de497ab670c22c103e59b7710990f
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-02-12 00:59:45 +0100

    Time: 0 ms (100.00%), Space: 43.3 MB (53.63%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.java
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -1,33 +1,17 @@
 class Solution {
-    public int[][] matrixReshape(int[][] mat, int r, int c) {        
-        if(mat.length*mat[0].length!=r*c)
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        if(mat.length * mat[0].length != c * r)
             return mat;
         int[][] res = new int[r][c];
-        int x =0, y=0;
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[i].length;j++){
+        int x = 0, y=0;
+        for(int i =0;i<mat.length;i++)
+            for(int j=0;j<mat[0].length;j++){
                 if(y == c){
                     y = 0;
-                    x++;
+                    x ++;
                 }
                 res[x][y++] = mat[i][j];
             }
-        }
         return res;
-        
-//         int[][] res = new int[r][c];
-//         if (nums.length == 0 || r * c != nums.length * nums[0].length)
-//             return nums;
-        
-//         Queue<Integer> queue = new LinkedList();
-//         for (int i = 0; i < nums.length; i++) 
-//             for (int j = 0; j < nums[0].length; j++) 
-//                 queue.add(nums[i][j]);
-                
-//         for (int i = 0; i < r; i++) 
-//             for (int j = 0; j < c; j++) 
-//                 res[i][j] = queue.remove();
-        
-//         return res;
     }
 }
