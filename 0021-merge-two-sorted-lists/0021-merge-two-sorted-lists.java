6a1ff8d25c8f56e75190feb75889a625a4f0f44b
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-02-12 00:19:22 +0100

    Time: 0 ms (100.00%), Space: 42.1 MB (37.33%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -9,35 +9,36 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null) 
-            return l2;
-		if(l2 == null) 
-            return l1;
-		if(l1.val < l2.val){
-			l1.next = mergeTwoLists(l1.next, l2);
-			return l1;
-		} else{
-			l2.next = mergeTwoLists(l1, l2.next);
-			return l2;
-		}
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null)
+            return list2;
+        if(list2 == null)
+            return list1;
+        if(list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else{
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+        
 //         ListNode head = new ListNode();
 //         ListNode temp = head;
-//         while(l1 != null && l2 != null){
-//             if(l1.val <= l2.val){
-//                 temp.next = l1;
-//                 l1 = l1.next;
+//         while(list1 != null && list2 != null){
+//             if(list1.val <= list2.val){
+//                 temp.next = list1;
+//                 list1 = list1.next;
 //             }else{
-//                 temp.next = l2;
-//                 l2 = l2.next;
+//                 temp.next = list2;
+//                 list2 = list2.next;
 //             }
 //             temp = temp.next;
 //         }
-//         if(l1 != null && l2 == null)
-//             temp.next = l1;
+//         if(list1 != null && list2 == null)
+//             temp.next = list1;
         
-//         if(l2 != null && l1 == null)
-//             temp.next = l2;
+//         if(list2 != null && list1 == null)
+//             temp.next = list2;
 //         return head.next;
     }
 }
