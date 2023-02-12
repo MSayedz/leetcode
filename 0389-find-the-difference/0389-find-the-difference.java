3386f4cb8763cc20a67e7012cce4fe009517fb97
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-02-13 00:00:11 +0100

    Time: 3 ms (45.33%), Space: 40.7 MB (58.27%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -3,14 +3,14 @@ class Solution {
         if(s.length()>t.length())
             return findTheDifference(t, s);
         
-        int[] nums = new int[128];
+        int[] nums = new int[256];
         
         for(int i=0;i<s.length();i++){
-            nums[s.charAt(i)-'a']++;
+            nums[s.charAt(i)]++;
         }
         for(int i=0;i<t.length();i++){
-            nums[t.charAt(i)-'a']--;
-            if(nums[t.charAt(i)-'a'] <0)
+            nums[t.charAt(i)]--;
+            if(nums[t.charAt(i)] <0)
                 return t.charAt(i);
         }
         return ' ';
