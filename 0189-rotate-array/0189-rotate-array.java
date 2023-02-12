ec47c8dd95ccd5d9ca2f08090e77f2dd0d6a6f08
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-02-12 16:52:53 +0100

    Time: 1 ms (64.22%), Space: 58.1 MB (81.82%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,33 +1,35 @@
 class Solution {
-//     public void rotate(int[] nums, int k) {
-//         if (nums == null || nums.length <2 || k < 0) 
-//             return;
-//         int length = nums.length;
-//         k = k % length;
+    public void rotate(int[] nums, int k) {
+        if (nums == null || nums.length <2 || k < 0) 
+            return;
+        int length = nums.length;
+        k = k % length;
         
-//         if (k == 0) 
-//             return;
+        if (k == 0) 
+            return;
         
-//         int[] temp = new int[length];
-//         for (int i=0; i<nums.length; i++) 
-//             temp[i] = nums[(i + length - k) % length];
-//         for (int i=0; i<nums.length; i++) 
-//             nums[i] = temp[i];
-//     }
+        int[] temp = new int[length];
+        
+        for (int i=0; i<nums.length; i++) 
+            temp[i] = nums[(i + length - k) % length];
+        
+        for (int i=0; i<nums.length; i++) 
+            nums[i] = temp[i];
+    }
     
-  public void rotate(int[] nums, int k) { 
-    k = k % nums.length;
-    reverse(nums, 0, nums.length - 1);
-    reverse(nums, 0, k - 1);
-    reverse(nums, k, nums.length - 1);
-  }
+//   public void rotate(int[] nums, int k) {
+//       k = k % nums.length;
+//       reverse(nums, 0, nums.length - 1);
+//       reverse(nums, 0, k - 1);
+//       reverse(nums, k, nums.length - 1);
+//   }
 
-  public void reverse(int[] nums, int l, int r) {
-      int temp;
-      while (l < r) {
-          temp = nums[l];
-          nums[l++] = nums[r];
-          nums[r--] = temp;
-      }
-  }
+//   public void reverse(int[] nums, int l, int r) {
+//       int temp;
+//       while (l < r) {
+//           temp = nums[l];
+//           nums[l++] = nums[r];
+//           nums[r--] = temp;
+//       }
+//   }
 }
