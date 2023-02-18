978828bc8d68b4a24a3016cc8953629c69df8249
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-02-18 13:28:10 +0100

    Time: 1 ms (100.00%), Space: 42.1 MB (91.77%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,16 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Set<Integer> seen = new HashSet<>();
-        for(int i =0;i<nums.length;i++){
-            if(!seen.add(nums[i])){
-                seen.remove(nums[i]);
-            }
-        }
-        return seen.iterator().next();
+        // Set<Integer> seen = new HashSet<>();
+        // for(int i =0;i<nums.length;i++){
+        //     if(!seen.add(nums[i])){
+        //         seen.remove(nums[i]);
+        //     }
+        // }
+        // return seen.iterator().next();
+        
+        int ans =0;
+        for(int num:nums)
+            ans ^= num;
+        return ans;
     }
 }
