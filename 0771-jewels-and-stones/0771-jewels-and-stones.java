ec96b4041e7eaa5c805d853bdc0fa5cadfa7d02f
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-02-23 20:45:10 +0100

    Time: 1 ms (89.55%), Space: 43.7 MB (5.62%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,16 +1,23 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        int[] stonesCount = new int[256];
+//         int[] stonesCount = new int[256];
         
-        for(char cur: stones.toCharArray()){
-            stonesCount[cur]++;
-        }
+//         for(char cur: stones.toCharArray())
+//             stonesCount[cur]++;
+        
+//         int res = 0;
+//         for(char cur: jewels.toCharArray())
+//             res +=stonesCount[cur];
+        
+//         return res;
         
         int res = 0;
-        for(char cur: jewels.toCharArray()){
-            res +=stonesCount[cur];
-        }
-        
+        Set setJ = new HashSet();
+        for (char j: jewels.toCharArray())
+            setJ.add(j);
+        for (char s: stones.toCharArray())
+            if (setJ.contains(s)) 
+                res++;
         return res;
     }
 }
