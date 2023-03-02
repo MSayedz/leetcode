2f5236bda5d98f47720ea86e5b000a79ebc26f05
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-03-02 22:10:35 +0100

    Time: 1 ms (99.94%), Space: 41.2 MB (26.57%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,15 +1,17 @@
 class Solution {
     public boolean detectCapitalUse(String word) {  
+        // return word.matches("[A-Z]*|.[a-z]*"); too slow
         if (word.length() < 2)
             return true;   
         int count = 0;
         for(char cur:word.toCharArray())
             if(cur - 'a' < 0)
                 count++;
-        if(count == word.length())
+        if(count == 0 || count == word.length())
             return true;
         if(count > 1)
             return false;
-        return count == 1 ? word.charAt(0) - 'a' < 0 : true;
+        
+        return word.charAt(0) - 'a' < 0;
     }
 }
