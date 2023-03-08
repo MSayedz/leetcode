04226767a879821dad049dbb9deac50a463836c0
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-03-08 15:53:47 +0100

    Time: 1 ms (99.41%), Space: 43.1 MB (57.37%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findCircleNum(int[][] M) {
+        boolean[] visited = new boolean[M.length]; 
+        int count = 0;
+        for(int i = 0; i < M.length; i++) {
+            if(!visited[i]) {
+                count++;
+                dfs(M, visited, i);
+            }
+        }
+        
+        return count;
+    }
+    
+    private void dfs(int[][] M, boolean[] visited, int i) {
+        for(int j = 0; j < M.length; j++) {
+            if(M[i][j] == 1 && !visited[j]) {
+                visited[j] = true;
+                dfs(M, visited, j); 
+            }
+        }
+    }
+}
