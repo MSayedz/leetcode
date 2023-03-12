fc60a95913f7e4d26b45d25551ddec4a43e63f7b
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-03-12 22:05:56 +0100

    Time: 1 ms (99.73%), Space: 40.5 MB (51.62%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,14 +1,30 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer, Integer> counts = new HashMap<>();
-        Set<Integer> set = new HashSet<>();
-        for(int cur:arr)
-            counts.put(cur, counts.getOrDefault(cur, 0)+1);
-        for(int i:counts.keySet()){
-            if(counts.get(i)>0 && !set.add(counts.get(i))){
-                return false;
+        // Map<Integer, Integer> counts = new HashMap<>();
+        // Set<Integer> set = new HashSet<>();
+        // for(int cur:arr)
+        //     counts.put(cur, counts.getOrDefault(cur, 0)+1);
+        // for(int i:counts.keySet()){
+        //     if(counts.get(i)>0 && !set.add(counts.get(i))){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        
+        Arrays.sort(arr);
+        HashSet<Integer> set = new HashSet<>();
+
+        int count = 1;
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] == arr[i-1]) 
+                count++;
+            else {
+                if(!set.add(count)) 
+                    return false;
+                count = 1;
             }
         }
-        return true;
+        
+        return set.contains(count) ? false :true;
     }
 }
