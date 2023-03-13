b156bd4de253b51d80aee3fc5b0c6bae2b67befb
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-03-13 23:37:44 +0100

    Time: 0 ms (100.00%), Space: 40.7 MB (16.06%) - LeetHub

--- 0266-palindrome-permutation/0266-palindrome-permutation.java
+++ 0266-palindrome-permutation/0266-palindrome-permutation.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean canPermutePalindrome(String s) {
-        Set<Character> charsSet = new HashSet<>();
-        for(int i=0; i<s.length(); i++)
-            if(!charsSet.add(s.charAt(i)))
-                charsSet.remove(s.charAt(i));
-        return charsSet.size()<2;
+//         Set<Character> charsSet = new HashSet<>();
+//         for(int i=0; i<s.length(); i++)
+//             if(!charsSet.add(s.charAt(i)))
+//                 charsSet.remove(s.charAt(i));
+//         return charsSet.size()<2;
         
-//         int[] counts = new int[256];
-//         for(char cur:s.toCharArray()){
-//             counts[cur]++;
-//         }
-//         boolean oddCountFound = false;
-//         for(int i=0;i<256;i++){
-//             if(counts[i] != 0 && counts[i]% 2 !=0)
-//                 if(oddCountFound)
-//                     return false;
-//                 else
-//                     oddCountFound = true;
-//         }
-//         return true;
+        int[] counts = new int[256];
+        for(char cur:s.toCharArray()){
+            counts[cur]++;
+        }
+        boolean oddCountFound = false;
+        for(int i=0;i<256;i++){
+            if(counts[i] != 0 && counts[i]% 2 !=0)
+                if(oddCountFound)
+                    return false;
+                else
+                    oddCountFound = true;
+        }
+        return true;
     }
 }
