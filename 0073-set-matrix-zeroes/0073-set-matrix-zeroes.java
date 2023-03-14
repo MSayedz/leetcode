53fdf102fabc84a8f345a3961f9a6f0abd40417c
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-03-14 01:36:33 +0100

    Time: 1 ms (78.55%), Space: 44 MB (54.76%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int[][] matrixCopy = new int[matrix.length][matrix[0].length];
+        
+        for(int i=0;i<matrix.length;i++)
+            for(int j=0;j<matrix[0].length;j++)
+                if(matrix[i][j]==0)
+                    dfs(matrixCopy, i, j);
+        
+        for(int i=0;i<matrix.length;i++)
+            for(int j=0;j<matrix[0].length;j++)
+                if(matrixCopy[i][j] == -1)
+                    matrix[i][j] = 0;
+        
+    }
+    
+    private void dfs(int[][] matrixCopy, int i, int j){
+        for(int k=0;k<matrixCopy.length;k++)
+            matrixCopy[k][j] = -1;
+        for(int k=0;k<matrixCopy[0].length;k++)
+            matrixCopy[i][k] = -1;
+    }
+}
