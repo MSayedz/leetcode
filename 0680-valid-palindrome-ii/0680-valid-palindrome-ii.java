19b43abb3fdff42ab4efcfc10b7981497547f50d
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-04-08 03:33:40 +0300

    Time: 7 ms (97.88%), Space: 43.4 MB (18.16%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        return validPalindrome(s, 0, s.length() - 1, false);
+    }
+    
+    private boolean validPalindrome(String s, int start, int end, boolean hasDeleted) {
+        if (start >= end) return true;
+        
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                if (hasDeleted) 
+                    return false;
+                else 
+                    return validPalindrome(s, start + 1, end, true) 
+                    || validPalindrome(s, start, end - 1, true);
+            }
+            
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
