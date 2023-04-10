adbc4c4d68d5305da84fc851d0d3b6793ae906f0
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-04-10 16:52:08 +0300

    Time: 4 ms (99.97%), Space: 43.1 MB (62.69%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,27 +1,27 @@
 class Solution {
     public String removeDuplicates(String S) {
-//         char[] res = S.toCharArray();
-//         int i=0;
-//         for(int j=0;j<res.length;i++,j++){
-//             res[i] = res[j];
-//             if(i>0 && res[i] == res[i-1])
-//                 i-=2;
-//         }
-        
-//         return new String(res,0,i);
-        Deque<Character> dequeue = new ArrayDeque<>();
-        
-        for(int i=0;i<S.length();i++){
-            if(i == 0 || dequeue.size() == 0 || dequeue.peekLast() != S.charAt(i))
-                dequeue.addLast(S.charAt(i));
-            else
-                dequeue.removeLast();
+        char[] res = S.toCharArray();
+        int i=0;
+        for(int j=0;j<res.length;i++,j++){
+            res[i] = res[j];
+            if(i>0 && res[i] == res[i-1])
+                i-=2;
         }
         
-        StringBuilder sb = new StringBuilder();
-        while(dequeue.size() != 0)
-            sb.append(dequeue.removeFirst());
+        return new String(res,0,i);
+//         Deque<Character> dequeue = new ArrayDeque<>();
         
-        return sb.toString();
+//         for(int i=0;i<S.length();i++){
+//             if(i == 0 || dequeue.size() == 0 || dequeue.peekLast() != S.charAt(i))
+//                 dequeue.addLast(S.charAt(i));
+//             else
+//                 dequeue.removeLast();
+//         }
+        
+//         StringBuilder sb = new StringBuilder();
+//         while(dequeue.size() != 0)
+//             sb.append(dequeue.removeFirst());
+        
+//         return sb.toString();
     }
 }
