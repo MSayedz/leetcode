377168f8e8ec33e1c09f9916f40bc4f41eddfecf
MSayedz <36796596+MSayedz@users.noreply.github.com>
2023-04-16 22:37:09 +0200

    Update 0200-number-of-islands.java

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -24,4 +24,50 @@ class Solution {
         fillLand(grid, i, j+1);
         
     }
+    
+//     BFS
+//     class Cell {
+//         int row; int col;
+        
+//         public Cell(int row, int col){
+//             this.row = row;
+//             this.col = col;
+//         }
+        
+//         public int getRow(){return this.row;}
+//         public int getCol(){return this.col;}
+//     }
+    
+//     int[] DIR = new int[]{0, 1, 0, -1, 0};
+    
+//     public int numIslands(char[][] grid) {
+//         int islandsCount = 0;
+//         Cell curCell = null;
+//         ArrayDeque<Cell> queue = new ArrayDeque<>();
+//         for(int i = 0;i<grid.length; i++){
+//             for(int j=0;j<grid[0].length; j++){
+//                 if(grid[i][j] == '1'){
+//                     islandsCount++;
+//                     queue.offer(new Cell(i,j));
+//                     while(!queue.isEmpty()){
+//                         curCell = queue.poll();
+//                         int row = curCell.getRow();
+//                         int col = curCell.getCol();
+//                         for(int k=0;k<4;k++){
+//                             int nr = row + DIR[k], nc = col + DIR[k+1];
+//                             if (nr <0 || nr == grid.length 
+//                                 || nc < 0 || nc == grid[0].length 
+//                                 || grid[nr][nc] != '1')
+//                                 continue;
+//                                 grid[nr][nc] = '0';
+//                                 queue.offer(new Cell(nr,nc));
+//                         }
+                            
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return islandsCount;
+//     }
 }
